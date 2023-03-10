b62828f46618be0c1e236f44afbc998f3fe7f9fb
Boss <anime.cula@mail.ru>
2023-03-11 01:54:57 +0700

    Практика - 3

--- js/main.js
+++ js/main.js
@@ -1,32 +1,21 @@
 let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        altText: "A pair of socks",
-        inStock: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-            }
-        ],
-        cart: 0
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  el: '#app',
+  data: {
+      product: "Socks",
+      image: "./assets/vmSocks-green-onWhite.jpg",
+      altText: "A pair of socks",
+      inStock: true,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+            variantId: 2234,
+            variantColor: 'green'
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        {
+            variantId: 2235,
+            variantColor: 'blue'
         }
-    }
- })
- 
+     ]
+     
+  }
+})
